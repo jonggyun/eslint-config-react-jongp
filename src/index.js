e01bf7c2db8644aae519646757677d469c07f02d
JongGyun Park <jonggyuni@gmail.com>
2020-04-30 23:20:30 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugin: ['react', 'prettier', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': 'error',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
